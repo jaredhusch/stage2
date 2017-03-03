438e91f105ce82a6519665687ab1c4bfdee06aec
Jared Husch <jared.husch@gmail.com>
2017-03-03 16:45:04 -0500

    fourth committ

--- Stage2Project.py
+++ Stage2Project.py
@@ -1,4 +1,7 @@
 #stage 2 project: Quiz on space
+
+#These are the blanks that will have to be replaced
+
 blanks = ["__1__", "__2__", "__3__", "__4__"]
 
 easy_questions = "What planet has the nickname, the Red Planet __1__? What is the biggest planet in our solar system __2__? What is the name of the galaxy Earth resides in __3__? What is the smallest planet in our solar system __4__?"
@@ -10,30 +13,71 @@ medium_answers = ["Andromeda", "Supernova", "8", "Black Hole"]
 hard_questions = "What type of galaxy is the most common in the Universe __1__? What is the closest star to the Sun __2__? How many billions of years old is the Universe __3__? What is the name of the brightest star in the night sky __4__?"
 hard_answers = ["Elliptical", "Alpha Centauri", "13.7", "Sirius"]
 
-print ("Welcome to my Space Fill in the Blanks Quiz!!!")
-user_input = raw_input("Please choose a difficulty level: easy, medium, or hard.")
+#Ask for user's name
+user_name = raw_input('What is your name?')
 
-#Takes user input to determine the level and prints out level questions
-def level(user_input):
-    if user_input.lower() == "easy":
-        print("You've chosen easy.")
-        return easy_questions[:]
-    elif user_input.lower() == "medium":
-        print("You've chosen medium.")
-        return medium_questions
-    elif user_input.lower() == "hard":
-        print("You've chosen hard.")
-        return hard_questions
-print(level(user_input))
+#Greet the user
+print ("Hello and welcome to my Space Fill in the Blanks Quiz!!!" + user_name + "!")
 
-# Takes user input and returns level answers
-def answers(user_input): 
-    if user_input == "easy":
-        return easy_answers
-    elif user_input == "medium":
-        return medium_answers
-    elif user_input == "hard":
-        return hard_answers
+def get_level():
+  level = raw_input('Please choose a level between easy, medium and hard')
+  if level == "easy" or level == "medium" or level == "hard":
+    return level
 
+#getting the quiz  
+def get_questions(game_level):
+  if game_level == "easy":
+      print "You chose level Easy."
+      return easy_questions
+      
+  elif game_level == "medium":
+      print "You chose level Medium."
+      return medium_questions
+      
+  elif game_level == "hard":
+      print "You chose level Hard."
+      return hard_questions
+      
+#getting quiz answers      
+def get_answers(game_level):
+  
+  if game_level == "easy":
+      return easy_answers
+      
+  elif game_level == "medium":
+      return medium_answers
+      
+  elif game_level == "hard":
+      return hard_answers
 
-print(answers(user_input)[1])
+def blank_in_blanks(word, blanks):
+    for blank in blanks:
+        if blank in word:
+            return blank
+    return None
+
+def play_game():
+	game_level= get_level()
+	questions= get_questions(game_level)
+	answers= get_answers(game_level)
+	replaced = []
+	count_answers=0
+	print questions
+	questions = questions.split()
+	for word in questions:
+		replacement = blank_in_blanks(word, blanks)
+		if replacement != None:
+			user_input = raw_input("What is the answer for blank: " + replacement+ " ")
+			while user_input != answers[count_answers]:
+				print "Your answer was wrong. Please enter a new answer: "
+				user_input== raw_input()
+			word= word.replace(replacement, user_input)
+			replaced.append(word)
+			count_answers= count_answers+1
+			print " ".join(replaced)
+		else:
+			replaced.append(word)
+	replaced = " ".join(replaced)
+	print "Congratulations, you have found all the answers!"
+
+print play_game()
